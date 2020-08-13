1b304ce05e3e589859c76666888e5db507449721
Tony Nguyen <tnguyen@etisalat.ae>
2020-08-13 23:29:17 +0400

    Fix Jekyll

---
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "http://localhost:4000/consensus-core/jekyll/update/2020/03/02/welcome-to-jekyll.html",
+        "teaser":null}]
